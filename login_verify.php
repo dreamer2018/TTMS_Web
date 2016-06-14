a6c4678ca0042f34644f3e45f2fd452d1c6c6589
dreamer2018 <zhoupans_mail@163.com>
2016-06-15 01:18:47 +0800

    修正手机号码数据类型错误

--- login_verify.php
+++ login_verify.php
@@ -14,15 +14,13 @@
 $username = $_POST["username"];
 $password = $_POST["password"];
 
-echo "用户名：", $username, "<br/>";
-echo "密码：", $password;
-
 /*
  * 对输入的内容进行校验
  */
 $username = htmlentities($username, ENT_QUOTES, "UTF-8");
 $password = htmlentities($password, ENT_QUOTES, "UTF-8");
-$password = md5($password.'linux');
+
+//$password = md5($password.'linux');
 
 /*
  * 对数据进行解析，得出身份类型
@@ -38,12 +36,10 @@ if ($username[0] == '1') {
     $identity = 0;
 }
 
-echo "<br/>", $identity;
-
 /*
  * 连接数据库，进行用户名，密码验证
  */
-if (!$identity) {
+if ($identity) {
 
     /*
      * 通过身份数，得到对应的数据表
@@ -54,9 +50,11 @@ if (!$identity) {
         case 3 : $DB_TABLE_NAME="employee"; break;      //售票员表
         default : $DB_TABLE_NAME="";
     }
+
     /*
      * 如果身份表为空则不进行数据库连接
      */
+
     if($DB_TABLE_NAME !=""){
 
         require_once "DB_login.php";
@@ -79,7 +77,8 @@ if (!$identity) {
         /*
          * 写SQL语句
          */
-        $query=" select passwd from ".$DB_TABLE_NAME." where emp_no=".$username;
+        $query=" select passwd from ".$DB_TABLE_NAME." where emp_no= \"".$username."\";";
+        echo "<br/>",$query;
         /*
          * 获取查询结果
          */
@@ -87,20 +86,32 @@ if (!$identity) {
         /*
          * 对查询结果进行验证
          */
+
         if(!$result){
             die("查询失败<br/>");
         }else{
+
             while ($row = $result->fetch_array()){
+
+                if($row["passwd"] == $password){
+                    echo "登陆成功";
+                }else{
+                    echo "用户名或密码不正确";
+                }
                 //校验成功
+
                 $_SESSION['user'] = $username;
                 $_SESSION['identity'] = $identity;
                 echo $row["passwd"],"<br/>";
 
-                isset($_SESSION['user']){
+                if(isset($_SESSION['user'])){
                     echo $_SESSION['user'];
                 }
             }
         }
+    }else{
+        echo "用户名或密码错误";
     }
+}else{
+    echo "用户名或密码错误";
 }
-?>

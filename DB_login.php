2d32d76c7734ff6d0da5af86472eca6ace748e0c
dreamer2018 <zhoupans_mail@163.com>
2016-06-14 23:08:14 +0800

    add

---
+++ DB_login.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhoupan
+ * Date: 6/14/16
+ * Time: 9:33 PM
+ */
+$DB_USER="root";
+$DB_PASSWD="zhoupan";
+$DB_HOST="localhost";
+$DB_PORT="3306";
+$DB_NAME="ttms_web";
+
+session_start();
+
---
+++ test/pget.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhoupan
+ * Date: 6/14/16
+ * Time: 9:00 PM
+ */

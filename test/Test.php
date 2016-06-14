73fcd9afe96d042b54412305e65a51eb54119c69
dreamer2018 <zhoupans_mail@163.com>
2016-06-14 23:09:18 +0800

    add

--- test/Test.php
+++ test/Test.php
@@ -5,3 +5,14 @@
  * Date: 6/14/16
  * Time: 7:58 PM
  */
+$url = "http://bbs.lampbrother.net";
+?>
+<html>
+<head>
+<meta http-equiv="refresh" content="1;
+url=<?php echo $url; ?>">
+</head>
+<body>
+页面只停留一秒……
+</body>
+</html>
--- test/pget.php
+++ test/pget.php
@@ -5,3 +5,8 @@
  * Date: 6/14/16
  * Time: 9:00 PM
  */
+$username = $_POST["username"];
+$password = $_POST["password"];
+echo "test";
+echo $username;
+echo $password;

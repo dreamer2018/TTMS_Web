7a240134e2776b08934b713a110e1b22a7db841c
dreamer2018 <zhoupans_mail@163.com>
2016-06-15 23:53:44 +0800

    修复部分bug

--- manager/movie_insert.php
+++ manager/movie_insert.php
@@ -1,8 +1,8 @@
 <?php
-    session_start();
-    if (!isset($_SESSION["username"]) || !isset($_SESSION["identity"])) {
-        die("<h1>非法访问</h1>");
-    }
+session_start();
+if (!isset($_SESSION["username"]) || !isset($_SESSION["identity"])) {
+    die("<h1>非法访问</h1>");
+}
 ?>
 <!doctype html>
 <html>
@@ -64,147 +64,159 @@
     <div class="main-wrap">
 
         <div class="crumb-wrap">
-            <div class="crumb-list"><i class="icon-font"></i><a href="/jscss/admin/design/">首页</a><span class="crumb-step">&gt;</span><a class="crumb-name" href="design.html">影片管理</a><span class="crumb-step">&gt;</span><span>添加影片</span></div>
+            <div class="crumb-list"><i class="icon-font"></i><a href="/jscss/admin/design/">首页</a><span
+                    class="crumb-step">&gt;</span><a class="crumb-name" href="design.html">影片管理</a><span
+                    class="crumb-step">&gt;</span><span>添加影片</span></div>
         </div>
         <div class="result-wrap">
             <div class="result-content">
-                <form action="design.html" method="post" id="myform" name="myform" enctype="multipart/form-data" method="get">
+                <form action="design.html" method="post" id="myform" name="myform" enctype="multipart/form-data"
+                      method="get">
                     <table class="insert-tab" width="100%" id="fid" cellpadding="0" cellspacing="0">
                         <tbody>
-                            <tr>
-                                <th width="120"><i class="require-red">*</i>影片类型：</th>
-                                <td>
-                                    <select name="type" id="type" class="required">
-                                        <?php
-                                          
-                                            require_once "../../conf/DB_login.php";
-                                            /*
-                                             * 连接数据库
-                                             */
-                                            $connect = new mysqli($DB_HOST, $DB_USER, $DB_PASSWD);
-                                            /*
-                                             * 如果连接失败，则直接结束
-                                            */
-                                            if (!$connect) {
-                                                die("Connect DataBase Error!<br/>");
-                                            }
+                        <tr>
+                            <th width="120"><i class="require-red">*</i>影片类型：</th>
+                            <td>
+                                <select name="type" id="type" class="required">
+                                    <?php
 
-                                            /*
-                                             * 选择数据库
-                                             */
-                                            $select = $connect->select_db($DB_NAME);
-                                            $query="select (id,type) from type ;";
-                                            $result = $connect ->query($query);
-                                            while ($row = $result->fetch_array()) {
-                                                echo "<option value=".$row["id"].">".$row["type"]."</option>"
-                                            }
-                                        ?>
-                                    </select>
-                                </td>
-                            </tr>
-                               <tr>
-                                <th width="120"><i class="require-red">*</i>影片等级：</th>
-                                <td>
-                                    <select name="level" id="catid" class="required">
-                                        <?php
-                    
-                                            require_once "../../conf/DB_login.php";
-                                            /*
-                                             * 连接数据库
-                                             */
-                                            $connect = new mysqli($DB_HOST, $DB_USER, $DB_PASSWD);
-                                            /*
-                                             * 如果连接失败，则直接结束
-                                            */
-                                            if (!$connect) {
-                                                die("Connect DataBase Error!<br/>");
-                                            }
+                                    require_once "../conf/DB_login.php";
+                                    /*
+                                     * 连接数据库
+                                     */
+                                    $connect = new mysqli($DB_HOST, $DB_USER, $DB_PASSWD);
+                                    /*
+                                     * 如果连接失败，则直接结束
+                                    */
+                                    if (!$connect) {
+                                        die("Connect DataBase Error!<br/>");
+                                    }
 
-                                            /*
-                                             * 选择数据库
-                                             */
-                                            $select = $connect->select_db($DB_NAME);
-                                            $query="select (id,type) from level ;";
-                                            $result = $connect ->query($query);
-                                            while ($row = $result->fetch_array()) {
-                                                echo "<option value=".$row["id"].">".$row["type"]."</option>"
-                                            }
-                                        ?>
-                                    </select>
-                                </td>
-                            </tr>
-                                <tr>
-                                <th width="120"><i class="require-red">*</i>语言：</th>
-                                <td>
-                                    <select name="langue" id="lang" class="required">
-                                        <?php
-                                           
-                                            require_once "../../conf/DB_login.php";
-                                            /*
-                                             * 连接数据库
-                                             */
-                                            $connect = new mysqli($DB_HOST, $DB_USER, $DB_PASSWD);
-                                            /*
-                                             * 如果连接失败，则直接结束
-                                            */
-                                            if (!$connect) {
-                                                die("Connect DataBase Error!<br/>");
-                                            }
+                                    /*
+                                     * 选择数据库
+                                     */
+                                    $select = $connect->select_db($DB_NAME);
+                                    $query = "select id,type from type;";
+                                    $result = $connect->query($query);
+                                    while ($row = $result->fetch_array()) {
+                                        echo "<option value=" . $row["id"] . ">" . $row["type"] . "</option>";
+                                    }
+                                    ?>
+                                </select>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th width="120"><i class="require-red">*</i>影片等级：</th>
+                            <td>
+                                <select name="level" id="catid" class="required">
+                                    <?php
 
-                                            /*
-                                             * 选择数据库
-                                             */
-                                            $select = $connect->select_db($DB_NAME);
-                                            $query="select (id,type) from lang ;";
-                                            $result = $connect ->query($query);
-                                            while ($row = $result->fetch_array()) {
-                                                echo "<option value=".$row["id"].">".$row["type"]."</option>"
-                                            }
-                                        ?>
-                                    </select>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th><i class="require-red">*</i>影片名称：</th>
-                                <td>
-                                    <input class="common-text required" id="title" name="title" size="20" value="" type="text">
-                                </td>
-                            </tr>
+                                    require_once "../conf/DB_login.php";
+                                    /*
+                                     * 连接数据库
+                                     */
+                                    $connect = new mysqli($DB_HOST, $DB_USER, $DB_PASSWD);
+                                    /*
+                                     * 如果连接失败，则直接结束
+                                    */
+                                    if (!$connect) {
+                                        die("Connect DataBase Error!<br/>");
+                                    }
 
-                            <tr>
-                                <th><i class="require-red">*</i>剧目图片url：</th>
-                                <td>
-                                    <input class="common-text required-red" id="Introduction" name="introduction" size="20" value="" type="text">
-                                </td>
-                            </tr>
-                            <tr>
-                                <th><i class="require-red">*</i>简介：</th>
-                                <td>
-                                    <input class="common-text required-red" id="Introduction" name="introduction" size="20" value="" type="text">
-                                </td>
-                            </tr>
-                            <tr>
-                                <th><i class="require-red">*</i>评分：</th>
-                                <td>
-                                    <input class="common-text required-red" id="Score" name="score" size="20" value="" type="text">
-                                </td>
-                            </tr>
-                            <tr>
-                                <th><i class="require-red">*</i>时间：</th>
-                                <td>
-                                    <input class="common-text required-red" id="time" name="title" size="20" value="" type="text">
-                                </td>
-                            </tr>
-                            <tr>
-                                <th><i class="require-red">*</i>价格：</th>
-                                <td>
-                                    <input class="common-text required-red" id="price" name="title" size="20" value="" type="text">
-                                </td>
-                            </tr>
-                           
-                        </tbody></table><br/>
-                        <input id="subid" class="btn btn-primary btn6 mr10" value="提交" type="submit" style="margin-left:6%;">
-                        <input class="btn btn6" onclick="history.go(-1)" value="返回" type="button" style="margin-left:3%">
+                                    /*
+                                     * 选择数据库
+                                     */
+                                    $select = $connect->select_db($DB_NAME);
+                                    $query = "select id,type from level ;";
+                                    $result = $connect->query($query);
+                                    while ($row = $result->fetch_array()) {
+                                        echo "<option value=" . $row["id"] . ">" . $row["type"] . "</option>";
+                                    }
+                                    ?>
+                                </select>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th width="120"><i class="require-red">*</i>语言：</th>
+                            <td>
+                                <select name="langue" id="lang" class="required">
+                                    <?php
+
+                                    require_once "../conf/DB_login.php";
+                                    /*
+                                     * 连接数据库
+                                     */
+                                    $connect = new mysqli($DB_HOST, $DB_USER, $DB_PASSWD);
+                                    /*
+                                     * 如果连接失败，则直接结束
+                                    */
+                                    if (!$connect) {
+                                        die("Connect DataBase Error!<br/>");
+                                    }
+
+                                    /*
+                                     * 选择数据库
+                                     */
+                                    $select = $connect->select_db($DB_NAME);
+                                    $query = "select id,type from lang ;";
+                                    $result = $connect->query($query);
+                                    while ($row = $result->fetch_array()) {
+                                        echo "<option value=" . $row["id"] . ">" . $row["type"] . "</option>";
+                                    }
+                                    ?>
+                                </select>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th><i class="require-red">*</i>影片名称：</th>
+                            <td>
+                                <input class="common-text required" id="title" name="title" size="20" value=""
+                                       type="text">
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <th><i class="require-red">*</i>剧目图片url：</th>
+                            <td>
+                                <input class="common-text required-red" id="Introduction" name="introduction" size="20"
+                                       value="" type="text">
+                            </td>
+                        </tr>
+                        <tr>
+                            <th><i class="require-red">*</i>简介：</th>
+                            <td>
+                                <input class="common-text required-red" id="Introduction" name="introduction" size="20"
+                                       value="" type="text">
+                            </td>
+                        </tr>
+                        <tr>
+                            <th><i class="require-red">*</i>评分：</th>
+                            <td>
+                                <input class="common-text required-red" id="Score" name="score" size="20" value=""
+                                       type="text">
+                            </td>
+                        </tr>
+                        <tr>
+                            <th><i class="require-red">*</i>时间：</th>
+                            <td>
+                                <input class="common-text required-red" id="time" name="title" size="20" value=""
+                                       type="text">
+                            </td>
+                        </tr>
+                        <tr>
+                            <th><i class="require-red">*</i>价格：</th>
+                            <td>
+                                <input class="common-text required-red" id="price" name="title" size="20" value=""
+                                       type="text">
+                            </td>
+                        </tr>
+
+                        </tbody>
+                    </table>
+                    <br/>
+                    <input id="subid" class="btn btn-primary btn6 mr10" value="提交" type="submit" style="margin-left:6%;">
+                    <input class="btn btn6" onclick="history.go(-1)" value="返回" type="button" style="margin-left:3%">
+                    <br/>
                 </form>
             </div>
         </div>

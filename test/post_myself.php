8471d5a8930558c4f57b7bf1f47f5190e83f7f59
dreamer2018 <zhoupans_mail@163.com>
2016-06-15 23:54:06 +0800

    修复部分bug

--- test/post_myself.php
+++ test/post_myself.php
@@ -5,3 +5,24 @@
  * Date: 6/15/16
  * Time: 6:38 PM
  */
+
+  if(isset($_POST['id']))
+  {
+      echo "有ID";
+      echo $_POST['id'];
+      echo $_POST['search'];
+  }
+  else
+  {
+      echo "没ID";
+  }
+  ?>
+<form  action="post_myself.php" method="POST">
+    <input type=text name="id"/>
+    <select name="search" >
+        <option value="18">全部</option>
+        <option value="19">英语</option>
+        <option value="20">国语</option>
+    </select>
+    <input type=submit value="go"/>
+</form>

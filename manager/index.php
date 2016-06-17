41f8e29fb6a761e0678c05eacb478deac923cba2
dreamer2018 <zhoupans_mail@163.com>
2016-06-17 22:47:30 +0800

    add

---
+++ manager/index.php
@@ -0,0 +1,83 @@
+<?php
+    require_once "../conf/conf.php"
+?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>光影人生-影院票务管理系统</title>
+    <link rel="stylesheet" type="text/css" href="css/common.css"/>
+    <link rel="stylesheet" type="text/css" href="css/main.css"/>
+</head>
+<body>
+
+<!-- 网页头部 -->
+<div class="topbar-wrap white">
+    <div class="topbar-inner clearfix">
+        <div class="topbar-logo-wrap clearfix">
+            <h1 class="topbar-logo none"><a href="index.html" class="navbar-brand">后台管理</a></h1>
+            <ul class="navbar-list clearfix">
+                <li style="font-size:20px; font-weight:bold;">光影人生</li>
+                <li style="font-size: 16px;font-style: italic">-影院票务管理系统</li>
+            </ul>
+        </div>
+        <div class="top-info-wrap">
+            <ul class="top-info-list clearfix">
+                <li><a href="#"><i class="icon-font">&#xe014;</i></a></li>
+                <li><a href="#"><i class="icon-font">&#xe059;</i></a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- 头部结束-->
+
+<div class="container clearfix">
+
+    <!-- 网页菜单栏-->
+    <div class="sidebar-wrap">
+        <div class="sidebar-title">
+            <h1>菜单</h1>
+        </div>
+        <div class="sidebar-content">
+            <ul class="sidebar-list">
+                <li>
+                    <a href="#"><i class="icon-font">&#xe003;</i>常用操作</a>
+                    <ul class="sub-menu">
+                        <li><a href="theater_manager.html"><i class="icon-font">&#xe044;</i>影厅管理</a></li>
+                        <li><a href="movie_insert.php"><i class="icon-font">&#xe034;</i>影片管理</a></li>
+                        <li><a href="seat_manager.php"><i class="icon-font">&#xe063;</i>座位管理</a></li>
+                        <li><a href="schedule_insert.html"><i class="icon-font">&#xe014;</i>演出计划管理</a></li>
+                        <li><a href="managerStatistic.php"><i class="icon-font">&#xe065;</i>财务管理</a></li>
+                        <li><a href="managerStatistic.php"><i class="icon-font">&#xe042;</i>票房统计</a></li>
+                        <li><a href="select.html"><i class="icon-font">&#xe033;</i>查询</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#"><i class="icon-font">&#xe018;</i>系统管理</a>
+                    <ul class="sub-menu">
+                        <li><a href="employee_manager.php"><i class="icon-font">&#xe017;</i>人事管理</a></li>
+                        <li><a href="change_passwd.html"><i class="icon-font">&#xe017;</i>密码重置</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <!-- 菜单栏结束-->
+
+
+    <div class="main-wrap">
+        <div class="result-wrap">
+            <div class="result-title">
+                <h1>欢迎使用光影人生影院售票管理系统</h1>
+            </div>
+            <div class="result-content">
+                <div class="short-wrap">
+                    <p>感谢以下开发人员的辛勤努力：</p><br/>
+                    <p><strong>周攀，肖赞，杜宇晖，左佳，刘幸，王豪帅</strong></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>

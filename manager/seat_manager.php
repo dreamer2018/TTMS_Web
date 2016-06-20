d39ed32d49d0017ddefa69a88af68264303d848b
dreamer2018 <zhoupans_mail@163.com>
2016-06-20 13:09:48 +0800

    座位管理

--- manager/seat_manager.php
+++ manager/seat_manager.php
@@ -1,4 +1,7 @@
-<!doctype html>
+<?php
+require_once "../conf/conf.php";
+?>
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
@@ -8,11 +11,11 @@
     <script type="text/javascript" src="js/houtai.js"></script>
 </head>
 <body>
-<!-- 网页头部 -->
+
+<!--网页头部-->
 <div class="topbar-wrap white">
     <div class="topbar-inner clearfix">
         <div class="topbar-logo-wrap clearfix">
-            <h1 class="topbar-logo none"><a href="index.html" class="navbar-brand">后台管理</a></h1>
             <ul class="navbar-list clearfix">
                 <li style="font-size:20px; font-weight:bold;">光影人生</li>
                 <li style="font-size: 16px;font-style: italic">-影院票务管理系统</li>
@@ -26,11 +29,13 @@
         </div>
     </div>
 </div>
-<!-- 头部结束-->
+<!--头部结束-->
 
 
 <div class="container clearfix">
-    <!-- 网页菜单栏-->
+
+
+    <!--网页菜单栏-->
     <div class="sidebar-wrap">
         <div class="sidebar-title">
             <h1>菜单</h1>
@@ -40,127 +45,174 @@
                 <li>
                     <a href="#"><i class="icon-font">&#xe003;</i>常用操作</a>
                     <ul class="sub-menu">
-                        <li><a href="design.html"><i class="icon-font">&#xe044;</i>影厅管理</a></li>
-                        <li><a href="design.html"><i class="icon-font">&#xe034;</i>影片管理</a></li>
-                        <li><a href="design.html"><i class="icon-font">&#xe063;</i>座位管理</a></li>
-                        <li><a href="design.html"><i class="icon-font">&#xe014;</i>演出计划管理</a></li>
-                        <li><a href="design.html"><i class="icon-font">&#xe065;</i>财务管理</a></li>
-                        <li><a href="design.html"><i class="icon-font">&#xe042;</i>票房统计</a></li>
-                        <li><a href="managerStatistic.html"><i class="icon-font">&#xe033;</i>查询</a></li>
+                        <li><a href="book_ticket.php"><i class="icon-font">&#xe044;</i>售票</a></li>
+                        <li><a href="return_ticket.php"><i class="icon-font">&#xe034;</i>退票</a></li>
+                        <li><a href="select_action.php"><i class="icon-font">&#xe063;</i>影片查询</a></li>
+                        <li><a href="schedule_select.php"><i class="icon-font">&#xe014;</i>演出计划查询</a></li>
+                        <li><a href="employeeStatistic.php"><i class="icon-font">&#xe065;</i>统计</a></li>
                     </ul>
                 </li>
                 <li>
                     <a href="#"><i class="icon-font">&#xe018;</i>系统管理</a>
                     <ul class="sub-menu">
-                        <li><a href="system.html"><i class="icon-font">&#xe017;</i>人事管理</a></li>
-                        <li><a href="system.html"><i class="icon-font">&#xe017;</i>密码重置</a></li>
+                        <li><a href="change_passwd.php"><i class="icon-font">&#xe017;</i>更改密码</a></li>
                     </ul>
                 </li>
             </ul>
         </div>
     </div>
-    <!-- 菜单栏结束-->
-    
+    <!--菜单栏结束-->
+
+
     <div class="main-wrap">
 
         <div class="crumb-wrap">
-            <div class="crumb-list"><i class="icon-font"></i><a href="index.html">首页</a><span class="crumb-step">&gt;</span><span class="crumb-name">影厅管理</span></div>
+            <div class="crumb-list"><i class="icon-font"></i><a href="index.php">首页</a><span
+                    class="crumb-step">&gt;</span><span class="crumb-name">座位管理</span></div>
+        </div>
+        <div class="result-wrap">
+            <div class="result-title">
+                <div class="result-list">
+                    <a href="movie_insert.php"><i class="icon-font"></i>添加</a>
+                </div>
+            </div>
         </div>
         <div class="search-wrap">
             <div class="search-content">
-                <form action="#" method="post">
+                <form action="seat_manager.php" method="post">
                     <table class="search-tab">
                         <tr>
-                            <th width="120">选择分类:</th>
+                            <th width="120">演出厅ID:</th>
                             <td>
-                                <select name="search-sort" >
-                                    <option value="">全部</option>
-                                    <option value="19">正在上映</option><option value="20">即将上映</option>
+                                <select name="studio_id">
+                                    <?php
+
+                                    /*
+                                     * 连接数据库
+                                     */
+                                    $connect = new mysqli($DB_HOST, $DB_USER, $DB_PASSWD);
+                                    /*
+                                     * 如果连接失败，则直接结束
+                                    */
+                                    if (!$connect) {
+                                        die("Connect DataBase Error!<br/>");
+                                    }
+
+                                    /*
+                                     * 选择数据库
+                                     */
+
+                                    $select = $connect->select_db($DB_NAME);
+
+                                    $query = "select id from studio;";
+                                    echo $query;
+                                    $result = $connect->query($query);
+                                    while ($row = $result->fetch_array()) {
+
+                                        echo "<option value=" . $row["id"] . ">" . $row["id"] . "</option>";
+                                    }
+                                    $connect->close();
+                                    ?>
                                 </select>
                             </td>
-                            <th width="70">关键字:</th>
-                            <td><input class="common-text" placeholder="关键字" name="keywords" value="" id="" type="text"></td>
-                            <td><input class="btn btn-primary btn2" name="sub" value="查询" type="submit"></td>
+                            <th width="50"></th>
+                            <td>
+                                <input class="btn btn-primary btn2" name="sub" value="查询" type="submit">
+                            </td>
                         </tr>
                     </table>
                 </form>
             </div>
         </div>
         <div class="result-wrap">
-            <form name="myform" id="myform" method="post">
-                <div class="result-title">
-                    <div class="result-list">
-                        <a href="insert.html"><i class="icon-font"></i>添加</a>
-                        <a id="batchDel" href="javascript:void(0)" ><i class="icon-font"></i>批量删除</a>
-                        <a id="updateOrd" href="javascript:void(0)"><i class="icon-font"></i>更新排序</a>
-                    </div>
-                </div>
-                <div class="result-content" id="fid">
-                    <table class="result-tab" width="100%" id="tableid" cellpadding="0" cellspacing="0">
-                        <tr>
-                            <th class="tc" width="5%"><input class="allChoose" name="" type="checkbox"></th>
-                            <th>排序</th>
-                            <th>ID</th>
-                            <th>影片名称</th>
-                            <th>影片类型</th>
-                            <th>放映厅</th>
-                            <th>状态</th>
-                            <th>时间</th>
-                            <th>票价</th>
-                            <th>操作</th>
-                        </tr>
-                        <tr>
-                            <td class="tc"><input name="id[]" value="59" type="checkbox"></td>
-                            <td>
-                                <input name="ids[]" value="59" type="hidden">
-                                <input class="common-input sort-input" name="ord[]" value="0" type="text">
-                            </td>
-                            <td>59</td>
-                            <td title="美国队长3"><a target="_blank" href="#" title="美国队长3">美国队长3</a> …
-                            </td>
-                            <td>IMAX</td>
-                            <td>2号厅</td>
-                            <td>正在上映</td>
-                            <td>5.11 21:11:01-5.11 23:11:01</td>
-                            <td></td>
-                            <td>
-                                <a class="link-update" href="#">修改</a>
-                                <a class="link-del" href="#" onclick="deleteRow(this)">删除</a>
-                            </td>
-                        </tr>
-                        <td class="tc"><input name="id[]" value="59" type="checkbox"></td>
-                            <td>
-                                <input name="ids[]" value="59" type="hidden">
-                                <input class="common-input sort-input" name="ord[]" value="0" type="text">
-                            </td>
-                            <td>59</td>
-                            <td title="美国队长3"><a target="_blank" href="#" title="美国队长3">美国队长3</a> …
-                            </td>
-                            <td>IMAX</td>
-                            <td>2号厅</td>
-                            <td>正在上映</td>
-                            <td>5.11 21:11:01-5.11 23:11:01</td>
-                            <td></td>
-                            <td>
-                                <a class="link-update" href="#">修改</a>
-                                <a class="link-del" href="#" onclick="deleteRow(this)">删除</a>
-                            </td>
-                        </tr>
-                      
-                    </table>
-                    <div class="list-page"> 1 条 1/1 页</div>
-                </div>
-            </form>
+            <div class="result-content" id="fid">
+                <?php
+                /*
+                 * 连接数据库
+                 */
+                $connect = new mysqli($DB_HOST, $DB_USER, $DB_PASSWD);
+                /*
+                 * 如果连接失败，则直接结束
+                */
+                if (!$connect) {
+                    die("Connect DataBase Error!<br/>");
+                }
+
+                /*
+                 * 选择数据库
+                 */
+
+                $select = $connect->select_db($DB_NAME);
+
+                if (isset($_POST['seat_id'])) {
+                    $query = "delete from ticket where seat_id =" . $_POST['seat_id'] . ";";
+                    $connect->query($query);
+                    $query = "delete from seat where id =" . $_POST['seat_id'] . ";";
+                    $connect->query($query);
+                }
+                if (isset($_POST['alter_id'])) {
+                    if ($_POST['alter'] == "启用") {
+                        $query = "update seat set status = 1 where id =" . $_POST['alter_id'] . ";";
+                        $connect->query($query);
+                    } else {
+                        $query = "update seat set status = 0 where id =" . $_POST['alter_id'] . ";";
+                        $connect->query($query);
+                    }
+                }
+                if (isset($_POST['studio_id'])) {
+
+
+                    $studio_id = $_POST['studio_id'];
+                    $query = "select id,row,col,status from seat where studio_id = " . $_POST['studio_id'] . ";";
+                    $result = $connect->query($query);
+                    if ($result) {
+
+                        echo " <table class=\"result-tab\" width=\"100%\" id=\"tableid\" cellpadding=\"0\" cellspacing=\"0\">";
+                        echo "<tr >";
+                        echo "<th >ID</th >";
+                        echo "<th >行</th >";
+                        echo "<th >列</th >";
+                        echo "<th >状态</th >";
+                        echo "<th >操作</th >";
+                        echo "<th >状态修改</th >";
+                        echo "</tr >";
+                        while ($row = $result->fetch_array()) {
+                            echo "<tr>";
+                            echo "<td>" . $row['id'] . "</td>";
+                            echo "<td>" . $row['row'] . "</td>";
+                            echo "<td>" . $row['col'] . "</td>";
+                            if ($row['status'] == 0) {
+                                echo "<td>" . 损坏 . "</td>";
+                            } else {
+                                echo "<td>" . 可用 . "</td>";
+                            }
+                            echo "<td>";
+                            echo "<form name=\"delete\" action=\"seat_manager.php\" id=\"myform\" method=\"post\">";
+                            echo "<input type=\"hidden\" name = \"seat_id\" value=" . $row['id'] . ">";
+                            echo "<input type=\"submit\" class=\"btn btn-primary btn2\" value=\"删除\">";
+                            echo "</form>";
+                            echo "</td>";
+                            echo "<td>";
+                            if ($row['status'] == 0) {
+                                echo "<form name=\"alter\" action=\"seat_manager.php\" id=\"myform\" method=\"post\">";
+                                echo "<input type=\"hidden\" name = \"alter_id\" value=" . $row['id'] . ">";
+                                echo "<input type=\"submit\" class=\"btn btn-primary btn2\" value=\"启用\" name =\"alter\" >";
+                                echo "</form>";
+                            } else {
+                                echo "<form name=\"alter\" action=\"seat_manager.php\" id=\"myform\" method=\"post\">";
+                                echo "<input type=\"hidden\" name = \"alter_id\" value=" . $row['id'] . ">";
+                                echo "<input type=\"submit\" class=\"btn btn-primary btn2\" value=\"停用\">";
+                                echo "</form>";
+                            }
+                            echo "</td>";
+                            echo "</tr>";
+                        }
+                    }
+                }
+                ?>
+            </div>
         </div>
     </div>
-    <!--/main-->
-    <script type="text/javascript">
-        function post()
-            {
-                forPost.action="DestinationPage.aspx";
-                forPost.submit();
-            }
-    </script>
 </div>
 </body>
 </html>

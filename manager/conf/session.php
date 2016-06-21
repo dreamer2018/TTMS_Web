48198206f9b1ea44595e885f10effb01ce8f0192
dreamer2018 <zhoupans_mail@163.com>
2016-06-21 23:29:50 +0800

    去掉配置文件bug

--- manager/conf/session.php
+++ manager/conf/session.php
@@ -7,7 +7,7 @@
  */
 session_start();
 if (!isset($_SESSION["username"]) || !isset($_SESSION["identity"])) {
-    die("<h1>非法访问</h1>");
-}elseif (substr($_SESSION['username'],0,1) != 2){
+    die("<h1>未登录，非法访问</h1>");
+}elseif ($_SESSION['username'] != 2){
     die("<h1>已登录账户类型不匹配，非法访问</h1>");
 }

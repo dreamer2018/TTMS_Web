60e5bba75669f1f4e96cf5059f9bfcedda7603c1
dreamer2018 <zhoupans_mail@163.com>
2016-06-21 22:58:19 +0800

    add

--- conf/session.php
+++ conf/session.php
@@ -8,4 +8,6 @@
 session_start();
 if (!isset($_SESSION["username"]) || !isset($_SESSION["identity"])) {
     die("<h1>非法访问</h1>");
+}elseif (substr($_SESSION['username'],0,1) != 3){
+    die("<h1>已登陆账户类型不匹配，非法访问</h1>");
 }

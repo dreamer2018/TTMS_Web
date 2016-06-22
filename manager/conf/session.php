1f1edf55d36eab2ea9c098157cb96f8c57b552c9
dreamer2018 <zhoupans_mail@163.com>
2016-06-22 22:03:18 +0800

    debug

--- manager/conf/session.php
+++ manager/conf/session.php
@@ -8,6 +8,6 @@
 session_start();
 if (!isset($_SESSION["username"]) || !isset($_SESSION["identity"])) {
     die("<h1>未登录，非法访问</h1>");
-}elseif ($_SESSION['username'] != 2){
+}elseif ($_SESSION['identity'] != 2){
     die("<h1>已登录账户类型不匹配，非法访问</h1>");
 }
